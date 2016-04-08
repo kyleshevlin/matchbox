23ee5676557a07bc78b4d53112f9a8357ebbdcdb
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2016-04-08 11:17:58 -0700

    convert to vanilla JS, update Readme

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
 # Matchbox
 
-A simple jQuery plugin for matching the height of boxes.
+A simple JavaScript plugin to match the height of boxes.
 
 ## Usage
 
 ```js
-$('.box-of-matches').matchbox({
+var matchbox = new Matchbox({
   selector: '.js-match',
-  groups_of: 2
+  groupsOf: 2
 });
+
+matchbox.init();
 ```
 
-After adding jQuery and `matchbox.js` to your project, call the `.matchbox()` function on a parent element to the elements you are targeting. The parent element merely serves to allow multiple calls of Matchbox on the page.
+After adding `matchbox.js` to your project, instantiate a new `Matchbox`, passing in your options object as the first argument, and call the `.init()` function on your Matchbox instance.
 
-Currently, there are two settings that you can update: `selector` and `groups_of`. For the `selector` setting, pass in the class name of the items you wish to match. This defaults to `.js-match` like you see above.
+## Options
 
-For the `groups_of` setting, pass in the number of items you want to match in each group. The default setting is 2 (You wouldn't need to match heights of a singular item, would you? (It's a paradox, don't think too hard about it or about nested parentheticals)).
+| Option Name | Type | Description | Default Value |
+|---|---|---|---|
+| selector | String | class or id selector | '.js-match' |
+| groupsOf | Integer | Size of groups to process for matches | 2 |
+
+## Public Methods
+
+| Method Name | Description |
+|---|---|
+| init() | initializes the Matchbox instance with |
+| groupsOf(number) | Quickly modify the groupsOf setting on a Matchbox instance |
 
 ## Future
 
-This is simply the MVP (minimum viable product). My plan is to add a `breakpoints` object in the settings to easily allow the `groups_of` setting to be changed per breakpoint. Fair warning, this will be designed to work mobile-first.
+This is simply the MVP (minimum viable product). My plan is to add a `breakpoints` object in the options to easily allow the `groupsOf` setting to be changed per breakpoint. Fair warning, this will be designed to work mobile-first.
 
 Matchbox will also respond to the `resize` event to ensure your boxes stay evenly matched. No point in setting a height and then letting some wrapping inner content break your boxes.
 
--- matchbox.js
+++ matchbox.js
@@ -1,68 +1,164 @@
-;(function($) {
+/*!
+ * Matchbox v1.0.0
+ * Match the height of boxes
+ * MIT License
+ */
 
-  $.fn.matchbox = function(options) {
+(function() {
+  'use strict';
 
-    // Default Settings
-    var settings = $.extend({
+  /**
+   * Matchbox
+   * @constructor
+   * @param {Object}
+   */
+  this.Matchbox = function() {
+    var defaults = {
       selector: '.js-match',
-      groups_of: 2
-    }, options);
+      groupsOf: 2
+    };
 
-    function getNextItems(items, number) {
-      return items.slice(0, number);
+    if ( arguments[0] && typeof arguments[0] === "object" ) {
+      this.options = extendDefaults(defaults, arguments[0]);
+    } else {
+      this.options = defaults;
     }
+  }
 
-    function getRestOfItems(items, number) {
-      return items.length <= number ? items : items.slice((items.length - number) * -1);
+  /**
+   * Matchbox init method; Initializes Matchbox with options passed into the new Constructor
+   * @access public
+   */
+  Matchbox.prototype.init = function() {
+    matchItems(createArrayFromNodesList(this.options.selector), this.options.groupsOf);
+  }
+
+  /**
+   * Update groupsOf option on the fly
+   * @access public
+   * @param {Integer} number
+   */
+  Matchbox.prototype.groupsOf = function(number) {
+    if ( !isNaN(number) ) {
+      this.options.groupsOf = number;
+      matchItems(createArrayFromNodesList(this.options.selector), this.options.groupsOf);
     }
+  }
 
-    function getTallestHeight(items) {
-      var tallestHeight = 0;
+  // Private Methods
 
-      items.each(function() {
-        // Clear previous height
-        $(this).css('height', '');
+  /**
+   * Create an array from a nodeList
+   * @access private
+   * @param {String} selector - Selector with which to query the DOM and create nodeList
+   * @returns {Array} Array of DOM nodes
+   */
+  function createArrayFromNodesList(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector));
+  }
 
-        if ( $(this).outerHeight() > tallestHeight ) {
-          tallestHeight = $(this).outerHeight();
-        }
-      });
+  /**
+   * Get the next set of items to process
+   * @access private
+   * @param {Array} items - Array of items
+   * @param {Integer} number - Size of next group of items
+   * @returns {Array} Array of the next set of items
+   */
+  function getNextItems(items, number) {
+    return items.slice(0, number);
+  }
 
-      return tallestHeight;
-    }
+  /**
+   * Get the rest of the items to process
+   * @access private
+   * @param {Array} items - Array of items
+   * @param {Integer} number - Size of group of items
+   * @returns {Array} Array of the rest of the items
+   */
+  function getRestOfItems(items, number) {
+    return items.length <= number ? items : items.slice((items.length - number) * -1);
+  }
 
-    function setSameHeight(items, height) {
-      items.each(function() {
-        $(this).css('height', height);
-      });
-    }
+  /**
+   * Get the height of the tallest item in the set
+   * @access private
+   * @param {Array} items - Array of items
+   * @returns {Integer} Height of tallest item
+   */
+  function getTallestHeight(items) {
+    var tallestHeight = 0;
 
-    function matchItems(items, number) {
-      if ( items.length === 0 ) { return; }
+    items.forEach(function(item, index, array) {
+      // Remove any previously set height
+      item.style.height = '';
 
-      var nextItems,
-          restOfItems,
-          tallestHeight;
-
-      nextItems = getNextItems(items, number);
-
-      if ( items.length > number ) {
-        restOfItems = getRestOfItems(items, number);
+      if ( item.offsetHeight > tallestHeight ) {
+        tallestHeight = item.offsetHeight;
       }
+    });
 
-      tallestHeight = getTallestHeight(nextItems);
+    return tallestHeight;
+  }
 
-      setSameHeight(nextItems, tallestHeight);
-
-      // If there are more items, match the next batch
-      if ( typeof restOfItems !== 'undefined' && restOfItems.length ) {
-        matchItems(restOfItems, number);
-      }
+  /**
+   * Set the same height on each item in the set
+   * @access private
+   * @param {Array} items - Array of items
+   * @param {Number|String} height - Height that each item will be set to
+   */
+  function setSameHeight(items, height) {
+    if ( !isNaN(height) ) {
+      height = height + 'px';
     }
 
-    matchItems($(this).find($(settings.selector)), settings.groups_of);
+    items.forEach(function(item, index, array) {
+      item.style.height = height;
+    });
+  }
 
-    return this;
-  };
+  /**
+   * Cycle through all groups of items, getting tallest height and setting that height on all items in that group
+   * @access private
+   * @param {Array} items - Array of items
+   * @param {Integer} number - Number of items in each group
+   */
+  function matchItems(items, number) {
+    if ( items.length === 0 ) { return; }
 
-}(jQuery));
+    var nextItems,
+        restOfItems,
+        tallestHeight;
+
+    nextItems = getNextItems(items, number);
+
+    if ( items.length > number ) {
+      restOfItems = getRestOfItems(items, number);
+    }
+
+    tallestHeight = getTallestHeight(nextItems);
+
+    setSameHeight(nextItems, tallestHeight);
+
+    if ( typeof restOfItems !== 'undefined' && restOfItems.length ) {
+      matchItems(restOfItems, number);
+    }
+  }
+
+  /**
+   * Utility method to extend defaults with user options
+   * @access private
+   * @param {Object} source - Object with default keys and values
+   * @param {Object} properties - Object with user options keys and values
+   * @returns {Object} An object of the merged options
+   */
+  function extendDefaults(source, properties) {
+    var property;
+
+    for (property in properties) {
+      if ( properties.hasOwnProperty(property) ) {
+        source[property] = properties[property];
+      }
+    }
+    return source;
+  }
+}());
